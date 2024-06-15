ef2f67ba60117b131c48100f3fd4adea139b2dd7
KrisnaMurti-21 <krisnamr27@gmail.com>
2024-06-15 07:03:34 +0700

    third commit

--- src/lib/firebase/init.ts
+++ src/lib/firebase/init.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyChBGkznHFZ9qCXzd0SInDaBTONU5WStww",
-  authDomain: "next-app-5b487.firebaseapp.com",
-  projectId: "next-app-5b487",
-  storageBucket: "next-app-5b487.appspot.com",
-  messagingSenderId: "321055964734",
-  appId: "1:321055964734:web:8e9cceb8cd9d04d70b5b6f",
-  measurementId: "G-R02LQ5LC9P",
+  apiKey: process.env.GOOGLE_FIREBASE_API_KEY,
+  authDomain: process.env.GOOGLE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.GOOGLE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.GOOGLE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.GOOGLE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.GOOGLE_FIREBASE_APP_ID,
+  measurementId: process.env.GOOGLE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
